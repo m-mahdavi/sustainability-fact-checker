a11a2cbab19cb4a75de08dabc1283638b30bb2de
amirfarahmand0 <amirhosein.farahmand.piruz@gmail.com>
2025-04-13 11:04:13 +0330

    images extracted from docs and added to image directory

--- tester.py
+++ tester.py
@@ -18,7 +18,8 @@ import source.document
     
 
 # Offline PDF
-url = "documents/Boeing.pdf"
+
+'''url = "documents/google/google_2024.pdf"
 doc = source.document.Document(url=url)
 content = doc.read_local_file()
 doc.content_type = "pdf"
@@ -30,4 +31,27 @@ for i, image in enumerate(extracted_images):
     file_path = os.path.join(output_folder, f"{i}.{image['ext']}")
     with open(file_path, "wb") as f:
         f.write(image["image_bytes"])
-    print(f"Saved {file_path}.")
+    print(f"Saved {file_path}.")'''
+    
+
+for year in range(2022, 2025): 
+    url = f"documents/cvs/cvs_{year}.pdf"
+    doc = source.document.Document(url=url)
+    content = doc.read_local_file()
+    doc.content_type = "pdf"
+    extracted_images = doc.extract_images(content)
+    file_name = os.path.basename(url)
+    folder_name = os.path.splitext(file_name)[0]
+    output_folder = os.path.join("images", folder_name)
+    
+    os.makedirs(output_folder, exist_ok=True)
+    
+
+    for i, image in enumerate(extracted_images):
+        file_path = os.path.join(output_folder, f"{i}.{image['ext']}")
+        with open(file_path, "wb") as f:
+            f.write(image["image_bytes"])
+        print(f"Saved {file_path}.")
+
+
+
